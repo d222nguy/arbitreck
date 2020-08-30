2d2e5104d0a83448b30c37eb156ee130eb00380a
Duy Nguyen <d222nguy@uwaterloo.ca>
2020-08-30 00:20:16 -0400

    remove favicon

--- src/RateDisplayer.js
+++ src/RateDisplayer.js
@@ -21,7 +21,7 @@ const RateDisplayer = () => {
             <h2 className="f6 black-70 fw2 ttu tracked">Los Angeles</h2> */}
             </header>
             <div className="flex items-center justify-center pa4 bg-light-gray">
-                <svg className="w1" viewBox="0 0 32 32" style={{fill:"currentcolor"}}>
+                <svg viewBox="0 0 32 32" style={{fill:"currentcolor"}}>
                     <path d="M16 0 A16 16 0 0 1 16 32 A16 16 0 0 1 16 0 M19 15 L13 15 L13 26 L19 26 z M16 6 A3 3 0 0 0 16 12 A3 3 0 0 0 16 6"></path>
                 </svg>
                 <span className="lh-solid ml3 f1">{amount} {first} = {isNaN(rate * amount) ? "[unset]" : rate * amount} {second}</span>
